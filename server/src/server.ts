f7b38c7c7ea4e9ada5979310bce1b48500f0de1f
dka0086 <getuliofariasegundo@gmail.com>
2026-08-12 18:14:44 -0300

    feature: metodo login e hashpassword implementados

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,18 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(session({
+  secret: 'senhasecreta123',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: process.env.NODE_ENV === 'production', maxAge: 24 * 60 * 60 * 1000 } // 24 hours
+}));
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+function session(arg0: {
+    secret: string; resave: boolean; saveUninitialized: boolean; cookie: { secure: boolean; maxAge: number } // 24 hours
+}): any {
+    throw new Error("Function not implemented.")
+}
