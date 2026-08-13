3884dfed00a1b62eca2bead489343b84ba82075d
dka0086 <getuliofariasegundo@gmail.com>
2026-08-13 17:49:28 -0300

    fix: ajustes locais antes do pull

--- server/src/controller/image.controller.ts
+++ server/src/controller/image.controller.ts
@@ -1,16 +1,22 @@
 import { Request, Response, NextFunction} from "express";
 import { randomUUID } from "crypto"
 import { upload } from "../middlewares/upload.middleware"
+import { z } from "zod"
 
 export class ImageController {
+    imageSchema = z.object({
+            fileName: z.string().min(2).max(70),
+            scale: z.number(),
+            size: z.string().min(5).max(50),
+        })
+
     public upscaleLanczos3 = (req: Request, res: Response, next: NextFunction) => {
-        const fileName = req.body //COMO EXTRAI O "file" do objeot!?!?!?!?
-        upload(req, )
-        jobObject = {
-            job_id: randomUUID(),
+        const jobId = randomUUID()
+        const jobObject = {
+            job_id: jobId,
             type: "Upscale",
-            image_path: ,
-            scale: float = Field(..., gt=0),
+            image_path: req.file?.path,
+            scale: this.imageSchema(),
             callback_url:
         } 
     }
