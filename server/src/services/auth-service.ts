eadb51b98c8809ae8c0b9867bd610a5e19666ddd
dka0086 <getuliofariasegundo@gmail.com>
2026-08-18 17:12:05 -0300

    fix: colocando dados imutaveis no token jwt

--- server/src/services/auth-service.ts
+++ server/src/services/auth-service.ts
@@ -12,23 +12,23 @@ export class AuthService {
     const saltRounds = 10;
     const salt = await bcrypt.genSalt(saltRounds);
     return await bcrypt.hash(password, salt);
-  };
+  }
 
   public login = async (email: string, password: string) => {
     try {
       if (password == null || email == null) {
-        throw new Error("Campos vazios.");
+        throw new Error("Campos vazios.")
       }
       const user = await this.repository.findByEmail(email);
       if (!user) {
-        throw new ConflictError("Email já cadastrado");
+        throw new ConflictError("Email já cadastrado")
       }
       const passwordMatches = await bcrypt.compare(password, user.password);
       if (!passwordMatches) {
             throw new UnauthorizedError("Email ou senha inválidos");
         }
       const token = jwt.sign(
-        { userId: user.id, email: user.email },
+        { userId: user.id, role: user.role },
         process.env.JWT_SECRET!,
         { expiresIn: "1h" },
       );
